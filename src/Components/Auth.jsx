40ecaf4bf0e57ac4a7e57d396681fba61175515b
Rahul <rahulsoniubr@gmail.com>
2023-04-05 01:00:37 +0530

    signup with custom email pass

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import {getAuth,createUserWithEmailAndPassword} from "firebase/auth";
 import app from "./firebase";
 
 const auth = getAuth(app);
-const flag = false;
 export default function Auth(props)
 {
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
    function onButtonClick(event)
    {
 
-    createUserWithEmailAndPassword(auth, "tisisthemyel@gmail.com", "thisispassword").then((value)=>{props.compM(event);}).catch((error)=>alert(error.code));
+    createUserWithEmailAndPassword(auth, email, password).then((value)=>{setEmail("");setPassword(""); props.compM(event);}).catch((error)=>alert(error.code));
     
    }
 
@@ -18,9 +19,9 @@ export default function Auth(props)
         <React.StrictMode>
            <h1> This is Auth </h1>
            <span> Email Id : </span>
-           <input type="email" placeholder="Your Email"/><p></p>
+           <input type="email" placeholder="Your Email" onChange={(e)=>setEmail(e.target.value)} value={email}/><p></p>
            <span> Password : </span>
-           <input type="password" placeholder="Password" />
+           <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
            <p></p>
            <button name="profileBtn" onClick={function(event){ onButtonClick(event)}}>Submit</button>
         </React.StrictMode>
