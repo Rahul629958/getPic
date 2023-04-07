633e3ac7836772decfc506c09b6adf1737f08239
Rahul <rahulsoniubr@gmail.com>
2023-04-07 07:15:34 +0530

    css auth done

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,49 +1,116 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import {getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword} from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "./firebase";
 
 const auth = getAuth(app);
-export default function Auth(props)
-{
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
-    const [isSignUp,setSignUp] = useState(true);
-    function changeFunc()
-    {
-        setSignUp(!isSignUp);
-        setEmail("");
-        setPassword("");
+export default function Auth(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSignUp, setSignUp] = useState(true);
+  function changeFunc() {
+    setSignUp(!isSignUp);
+    setEmail("");
+    setPassword("");
+  }
+  function onButtonClick(event) {
+    if (isSignUp) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((value) => {
+          signInWithEmailAndPassword(auth, email, password);
+          setEmail("");
+          setPassword("");
+          props.compM(event);
+        })
+        .catch((error) => alert(error.code));
+    } else {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((value) => {
+          setEmail("");
+          setPassword("");
+          props.compM(event);
+        })
+        .catch((error) => alert(error.code));
     }
-   function onButtonClick(event)
-   {
-     if(isSignUp){
-     createUserWithEmailAndPassword(auth, email, password).then((value)=>{signInWithEmailAndPassword(auth,email,password); setEmail("");setPassword(""); props.compM(event);}).catch((error)=>alert(error.code));
-     }else{
-    signInWithEmailAndPassword(auth,email,password).then( (value)=>{setEmail("");setPassword(""); props.compM(event);}).catch((error)=>alert(error.code))};
-   }
+  }
 
-    return (
-        <React.StrictMode>
-           <h1> {isSignUp?"SignUp":"LogIn"} </h1>
-           <span> Email Id : </span>
-           <input type="email" placeholder="Your Email" onChange={(e)=>setEmail(e.target.value)} value={email}/><p></p>
-           <span> Password : </span>
-           <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
-           <p></p>
-           <button name="profileBtn" onClick={function(event){ onButtonClick(event)}}>{isSignUp?"SignUp":"LogIn"}</button>
-           <p hidden={isSignUp?false:true}>Already have an account? <span onClick={changeFunc} >LogIn</span></p>
-           <p hidden={isSignUp}>Don't have an account? <span onClick={changeFunc}>SignUp</span></p>
-        </React.StrictMode>
-    );
+  return (
+    <React.StrictMode>
+      <div className="signUpLogIn container">
+        <h1> {isSignUp ? "SIGNUP" : "LOGIN"} </h1>
+        <div className="inputSection">
+        {/* <span> Email Id :</span> */}
+
+
+        <div class="input-group mb-3">
+           <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1">✉</span>
+           </div>
+          <input type="email" class="form-control" placeholder="Enter your email" aria-label="" aria-describedby="basic-addon1"
+                 onChange={(e) => setEmail(e.target.value)}
+                 value={email}/>
+        </div>
+
+
+        <div class="input-group mb-3">
+           <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1">🔑</span>
+           </div>
+          <input type="password" class="form-control" placeholder="Your Password here" aria-label="" aria-describedby="basic-addon1"
+                 onChange={(e) => setPassword(e.target.value)}
+                 value={password}/>
+        </div>
+       
+       </div>
+
+
+        {/* <input
+          type="email"
+          className="emailInput"
+          placeholder="Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <p></p>
+        <span> Password : </span>
+        <input
+          type="password"
+          className="passwordInput"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        </div> */}
+        <div className="btnSection">
+          {" "}
+          <button
+            name="profileBtn"
+            onClick={function (event) {
+              onButtonClick(event);
+            }}
+            className="btn btn-outline-light"
+          >
+            {isSignUp ? "SignUp" : "Login"}
+          </button>
+          <p hidden={isSignUp ? false : true}>
+            Already have an account? <span className="logSigClick" onClick={changeFunc}>LogIn</span>
+          </p>
+          <p hidden={isSignUp}>
+            Don't have an account? <span className="logSigClick" onClick={changeFunc}>SignUp</span>
+          </p>
+        </div>
+      </div>
+    </React.StrictMode>
+  );
 }
-
-
-
 
 // signInWithEmailAndPassword(auth, email, password)
 //   .then((userCredential) => {
-//     // Signed in 
+//     // Signed in
 //     const user = userCredential.user;
 //     // ...
 //   })
